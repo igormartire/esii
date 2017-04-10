42a0eb94b8a4588b84f2c93cba149da7df66d129
btzlopes <btzlopes@gmail.com>
2017-04-09 23:42:52 -0300

    documentação e chs 10

---
+++ move.py
@@ -0,0 +1,21 @@
+﻿#Função de movimentação no xadrez
+
+board = [['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'],
+         ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
+         ['.', '.', '.', '.', '.', '.', '.', '.'],
+         ['.', '.', '.', '.', '.', '.', '.', '.'],
+         ['.', '.', '.', '.', '.', '.', '.', '.'],
+         ['.', '.', '.', '.', '.', '.', '.', '.'],
+         ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
+         ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R']]
+
+
+def move (board, source_x, source_y, dest_x, dest_y):
+    piece_source = board[dest_x][dest_y]
+    piece_dest = board[source_x][source_y]
+    board[source_x][source_y] = piece_source
+    board[dest_x][dest_y] = piece_dest
+    print (board)
+
+move (board,7,4,5,5)
+

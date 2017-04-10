0a970d35a0cb7b50281a60a802f1bdfc3165837d
Igor Martire <igormartire@berkeley.edu>
2017-04-10 09:47:09 -0300

    :rocket: CHS-30 Implement color_board function

--- src/board_color.py
+++ src/board_color.py
@@ -1,30 +1,27 @@
+from commons.models import Coordinate
+from commons.utils import *
+
 WHITE = 0
 BLACK = 1
 GREEN = 2
 RED = 3
 
-def board_color(board, possible_destinations):
-    colored_board = []
-    next_color = BLACK
-    for l in range(8):
-        next_color = WHITE if next_color == BLACK else BLACK
-        row = []
-        for c in range(8):
-            row.append(next_color)
-            next_color = WHITE if next_color == BLACK else BLACK
-        colored_board.append(row)
+# Example: colored_board = color_board(board, [Coordinate(1,1), Coordinate(3,5)])
+def color_board(board, possible_destinations):
+    colored_board = [
+        [0, 1, 0, 1, 0, 1, 0, 1],
+        [1, 0, 1, 0, 1, 0, 1, 0],
+        [0, 1, 0, 1, 0, 1, 0, 1],
+        [1, 0, 1, 0, 1, 0, 1, 0],
+        [0, 1, 0, 1, 0, 1, 0, 1],
+        [1, 0, 1, 0, 1, 0, 1, 0],
+        [0, 1, 0, 1, 0, 1, 0, 1],
+        [1, 0, 1, 0, 1, 0, 1, 0]]
     
     for dst in possible_destinations:
-        if board[dst['l']][dst['c']] != '.':
-            colored_board[dst['l']][dst['c']] = RED
+        if board[dst.row][dst.column] != '.':
+            colored_board[dst.row][dst.column] = RED
         else:
-            colored_board[dst['l']][dst['c']] = GREEN
+            colored_board[dst.row][dst.column] = GREEN
 
     return colored_board
-
-board = [['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'], ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'], ['.', '.', '.', '.', '.', '.', '.', '.'], ['.', '.', '.', '.', '.', '.', '.', '.'], ['.', '.', '.', '.', '.', '.', '.', '.'], ['.', '.', '.', '.', '.', '.', '.', '.'], ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'], ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R']]
-
-r = board_color(board, [{'l': 1,'c': 1}, {'l': 3,'c': 5}])
-
-for row in r:
-    print(row)

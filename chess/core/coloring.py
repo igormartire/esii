7d29c9822a6f55cebd5863f915dc88a7c025463f
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2017-05-02 20:05:41 -0300

    :bug: Change coloring to return .value of Enum

--- chess/core/coloring.py
+++ chess/core/coloring.py
@@ -7,8 +7,9 @@ from chess.core.models import Color, Piece
 #   [models.Coordinate(1,1), models.Coordinate(3,5)]
 # )
 def color_board(board, possible_destinations):
-    B = Color.BLACK
-    W = Color.WHITE
+    # TODO: verificar se isso é mesmo necessário ou se estamos dando voltas a toa
+    B = Color.BLACK.value
+    W = Color.WHITE.value
 
     colored_board = [[W, B, W, B, W, B, W, B],
                      [B, W, B, W, B, W, B, W],
@@ -21,9 +22,9 @@ def color_board(board, possible_destinations):
 
     for dest in possible_destinations:
         if board[dest.row][dest.column] != Piece.NONE:
-            print(board[dest.row][dest.column])
-            colored_board[dest.row][dest.column] = Color.RED
+            #print(board[dest.row][dest.column])
+            colored_board[dest.row][dest.column] = Color.RED.value
         else:
-            colored_board[dest.row][dest.column] = Color.GREEN
+            colored_board[dest.row][dest.column] = Color.GREEN.value
 
     return colored_board

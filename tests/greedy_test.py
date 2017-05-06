e5b2524739e15aaa7ab94bda298ac6b91e53e72f
Igor Martire <igorx94@gmail.com>
2017-05-06 17:58:55 -0300

    :art: CHS-240 Fix code style

--- tests/greedy_test.py
+++ tests/greedy_test.py
@@ -2,6 +2,7 @@ from chess.ai.greedy import greedy_move
 from chess.core.models import Piece, Coordinate
 from chess.core.utils import empty_board
 
+
 def test_greedy_move_chooses_dumb_eating_move():
     board = empty_board()
 

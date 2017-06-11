09dbe99ba378e1a6758ad1244669a929d85ecff7
Igor Martire <igorx94@gmail.com>
2017-06-11 18:30:48 -0300

    :rocket: #80 Implement draw

--- chess/core/possible_destinations.py
+++ chess/core/possible_destinations.py
@@ -1,7 +1,10 @@
-from chess.core.models import Coordinate, Piece, Player
+from chess.core.models import Coordinate, Piece, Player, Color
 from chess.core.utils import (BLACK_PIECES, WHITE_PIECES,
-                              initial_board, piece_at, empty_at)
+                              initial_board, piece_at, empty_at,
+                              remaining_pieces, color_by_pos,
+                              get_piece_coordinate)
 from chess.core.moving import diagonal_moves, move
+from chess.core.coloring import color_board
 import copy
 
 
@@ -34,11 +37,9 @@ def is_check_for_player(game, checked_player):
     king = (Piece.WHITE_KING
             if checked_player == Player.WHITE
             else Piece.BLACK_KING)
-    for row in range(8):
-        for column in range(8):
-            pos = Coordinate(row, column)
-            if piece_at(game.board, pos) == king:
-                return is_attacked(game, pos, checked_player)
+    king_pos = get_piece_coordinate(game.board, king)
+    if (king_pos is not None):
+        return is_attacked(game, king_pos, checked_player)
 
 
 def is_check_mate_for_player(game, player):
@@ -52,7 +53,68 @@ def is_check_mate_for_player(game, player):
             if piece_at(game.board, pos) in pieces:
                 if len(destinations(game, pos)) > 0:
                     return False
-    return True
+    # checkmate only when check and also no possible moves
+    return is_check_for_player(game, player)
+
+
+def is_stalemate_for_player(game, player):
+    pieces = (WHITE_PIECES
+              if player == Player.WHITE
+              else BLACK_PIECES)
+
+    for row in range(8):
+        for column in range(8):
+            pos = Coordinate(row, column)
+            if piece_at(game.board, pos) in pieces:
+                if len(destinations(game, pos)) > 0:
+                    return False
+    # stalemate only when not in check but no possible moves
+    return not is_check_for_player(game, player)
+
+
+def impossible_check_mate(game):
+    king_versus_king = False
+    king_and_bishop_versus_king = False
+    king_and_knight_versus_king = False
+    king_and_bishop_versus_king_and_bishop_on_same_color = False
+    remaining_white_pieces = remaining_pieces(game.board, Color.WHITE)
+    remaining_black_pieces = remaining_pieces(game.board, Color.BLACK)
+    if len(remaining_white_pieces) > 2 or len(remaining_black_pieces) > 2:
+        return False
+    if len(remaining_white_pieces) == 1 and len(remaining_black_pieces) == 1:
+        king_versus_king = True
+    elif (len(remaining_white_pieces) == 1 and
+             set(remaining_black_pieces) == set([Piece.BLACK_KING,
+                                                 Piece.BLACK_BISHOP])):
+        king_and_bishop_versus_king = True
+    elif (len(remaining_black_pieces) == 1 and
+             set(remaining_white_pieces) == set([Piece.WHITE_KING,
+                                                 Piece.WHITE_BISHOP])):
+        king_and_bishop_versus_king = True
+    elif (len(remaining_white_pieces) == 1 and
+             set(remaining_black_pieces) == set([Piece.BLACK_KING,
+                                                 Piece.BLACK_KNIGHT])):
+        king_and_knight_versus_king = True
+    elif (len(remaining_black_pieces) == 1 and
+             set(remaining_white_pieces) == set([Piece.WHITE_KING,
+                                                 Piece.WHITE_KNIGHT])):
+        king_and_knight_versus_king = True
+    elif (set(remaining_white_pieces) == set([Piece.WHITE_KING,
+                                              Piece.WHITE_BISHOP])
+          and
+          set(remaining_black_pieces) == set([Piece.BLACK_KING,
+                                              Piece.BLACK_BISHOP])):
+        white_bishop_pos = get_piece_coordinate(game.board, Piece.WHITE_BISHOP)
+        black_bishop_pos = get_piece_coordinate(game.board, Piece.BLACK_BISHOP)
+        color_white_bishop_on = color_by_pos(white_bishop_pos)
+        color_black_bishop_on = color_by_pos(black_bishop_pos)
+        king_and_bishop_versus_king_and_bishop_on_same_color = (
+            color_white_bishop_on == color_black_bishop_on)
+
+    return (king_versus_king or
+            king_and_bishop_versus_king or
+            king_and_knight_versus_king or
+            king_and_bishop_versus_king_and_bishop_on_same_color)
 
 
 def is_attacked(game, pos, attacked_player):
@@ -63,8 +125,8 @@ def is_attacked(game, pos, attacked_player):
         for column in range(8):
             coord = Coordinate(row, column)
             piece = piece_at(game.board, coord)
-            if (piece in attacker_pieces and
-                    pos in destinations(game, coord, offensive_only=True)):
+            if (piece in attacker_pieces
+                and pos in destinations(game, coord, offensive_only=True)):
                 return True
 
 
--- chess/core/utils.py
+++ chess/core/utils.py
@@ -1,4 +1,4 @@
-from chess.core.models import Piece
+from chess.core.models import Piece, Color, Coordinate
 
 _ = Piece.NONE
 P = Piece.WHITE_PAWN
@@ -30,14 +30,14 @@ WHITE_PIECES = [Piece.WHITE_PAWN,
 
 
 def initial_board():
-    return [[r, n, b, q, k, b, n, r],
-            [p, p, p, p, p, p, p, p],
+    return [[_, _, _, _, _, _, _, _],
+            [_, _, _, k, _, _, _, _],
             [_, _, _, _, _, _, _, _],
             [_, _, _, _, _, _, _, _],
             [_, _, _, _, _, _, _, _],
             [_, _, _, _, _, _, _, _],
-            [P, P, P, P, P, P, P, P],
-            [R, N, B, Q, K, B, N, R]]
+            [_, _, _, _, K, Q, Q, _],
+            [_, _, _, _, _, _, _, _]]
 
 
 def empty_board():
@@ -68,3 +68,34 @@ def print_board(board):
         for piece in row:
             print(piece.value, end='')
         print()
+
+
+def remaining_pieces(board, color=None):
+    if color == None:
+        color_pieces = WHITE_PIECES + BLACK_PIECES
+    elif color == Color.WHITE:
+        color_pieces = WHITE_PIECES
+    elif color == Color.BLACK:
+        color_pieces = BLACK_PIECES
+    else:
+        return []
+
+    remaining_pieces = []
+    for row in board:
+        for piece in row:
+            if piece in color_pieces:
+                remaining_pieces.append(piece)
+    return remaining_pieces
+
+
+def color_by_pos(pos):
+    return Color.WHITE if (pos.row + pos.column) % 2 == 0 else Color.BLACK
+
+
+def get_piece_coordinate(board, piece):
+    for row in range(8):
+        for column in range(8):
+            pos = Coordinate(row, column)
+            if piece_at(board, pos) == piece:
+                return pos
+    return None

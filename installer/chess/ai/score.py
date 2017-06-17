ffe7ac13156ca3bfeb6ee606ca433e4ba8c62459
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2017-06-17 15:27:40 -0300

    :memo: Some additional files

---
+++ installer/chess/ai/score.py
@@ -0,0 +1,26 @@
+from chess.core.models import Piece
+
+PIECES_SCORE = {
+    Piece.NONE: 0,
+    Piece.WHITE_PAWN: -1,
+    Piece.WHITE_BISHOP: -3,
+    Piece.WHITE_KNIGHT: -3,
+    Piece.WHITE_ROOK: -5,
+    Piece.WHITE_QUEEN: -9,
+    Piece.WHITE_KING: -9001,
+    Piece.BLACK_PAWN: 1,
+    Piece.BLACK_BISHOP: 3,
+    Piece.BLACK_KNIGHT: 3,
+    Piece.BLACK_ROOK: 5,
+    Piece.BLACK_QUEEN: 9,
+    Piece.BLACK_KING: 9001
+}
+
+
+def score_board(board):
+    cpu_score = 0
+    for row in range(8):
+        for column in range(8):
+            piece = board[row][column]
+            cpu_score += PIECES_SCORE[piece]
+    return cpu_score
---
+++ installer/chess/core/coloring.py
@@ -0,0 +1,30 @@
+from chess.core.models import Color, Piece
+
+
+# Example:
+# colored_board = color_board(
+#   board,
+#   [models.Coordinate(1,1), models.Coordinate(3,5)]
+# )
+def color_board(board, possible_destinations):
+    # TODO: verificar se isso é mesmo necessário ou se estamos dando voltas a
+    # toa
+    B = Color.BLACK
+    W = Color.WHITE
+
+    colored_board = [[W, B, W, B, W, B, W, B],
+                     [B, W, B, W, B, W, B, W],
+                     [W, B, W, B, W, B, W, B],
+                     [B, W, B, W, B, W, B, W],
+                     [W, B, W, B, W, B, W, B],
+                     [B, W, B, W, B, W, B, W],
+                     [W, B, W, B, W, B, W, B],
+                     [B, W, B, W, B, W, B, W]]
+
+    for dest in possible_destinations:
+        if board[dest.row][dest.column] != Piece.NONE:
+            colored_board[dest.row][dest.column] = Color.RED
+        else:
+            colored_board[dest.row][dest.column] = Color.GREEN
+
+    return colored_board
